abe3ed38c1047d857b34e63eccb31ed39c1c9fb4
Hina Khan <47082795+Hana-Khan@users.noreply.github.com>
2023-10-05 22:10:30 +0500

    Initial Commit

---
+++ DevConnect/DevConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevConnectApp.swift
+//  DevConnect
+//
+//  Created by Hina Khan on 05/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct DevConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
